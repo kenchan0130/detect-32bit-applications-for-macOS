0b2ac19f33eefbb2dfb851029f08c594f8dd579d
Tadayuki Onishi <tonishi@folio-sec.com>
2019-10-09 14:25:29 +0900

    add system app include option

--- README.md
+++ README.md
@@ -3,9 +3,23 @@
 32-bit applications will not work from macOS Catalina.
 Therefore, listing 32bit applications is effective.
 
+## Usage
+
+If you want to execte at local, you can execute following command like:
+
+```sh
+$ ./detect_32bit_applications.py
+```
+
+And if you want to include system apps, you should change `IGNORE_SYSTEM_APP` of script from `True` to `False` like:
+
+```python
+IGNORE_SYSTEM_APP = False
+```
+
 ## Setup for Jamf Pro
 
-1. Access https://<JAMF_PRO_SERVER_HOST>>/computerExtensionAttributes.html
+1. Access https://<JAMF_PRO_SERVER_HOST>/computerExtensionAttributes.html
 2. Click "New" button
 3. Set any name to "DISPLAY NAME"
 4. Select "INVENTORY DISPLAY" to your liking
--- detect_32bit_applications.py
+++ detect_32bit_applications.py
@@ -4,6 +4,20 @@
 import plistlib
 import subprocess
 import platform
+import re
+import sys
+import codecs
+
+
+# System application is excluded by default because it will not be 32bit due to version upgrade.
+# If you want to get system app, you can change this variable to 'False'.
+IGNORE_SYSTEM_APP = True
+
+
+# Some apps may have some utf-8 charactors.
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+sys.stderr = codecs.getwriter("utf-8")(sys.stderr)
+
 
 def formatApplicationForConsole(application):
     path = application["path"]
@@ -13,6 +27,7 @@ def formatApplicationForConsole(application):
     else:
     	return path
 
+
 result = ""
 # Confirmation is made because it affects macOS Catalina (10.15) and below.
 if int(platform.mac_ver()[0].split(".")[1]) < 15:
@@ -21,6 +36,9 @@ if int(platform.mac_ver()[0].split(".")[1]) < 15:
 
     all_applicatons = sp_applications_data_type[0]["_items"]
     finded_32_bit_applications = [application for application in all_applicatons if application["has64BitIntelCode"] == "no"]
+    if IGNORE_SYSTEM_APP:
+        finded_32_bit_applications = [application for application in finded_32_bit_applications if not re.match(r"^/System", application["path"])]
+
     result = "\n".join(map(formatApplicationForConsole, finded_32_bit_applications))
 
 print("<result>%s</result>" % result)
